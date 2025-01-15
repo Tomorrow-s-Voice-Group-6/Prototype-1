224777634f1ac0c78275be30c0ac12a1268658af
CarterWilliams7 <cwilliams54@ncstudents.niagaracollege.ca>
2025-01-14 20:08:27 -0500

    reset project to configure for https and for indivudal accounts

--- Peacock-Solutions-P1/Peacock-Solutions-P1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Peacock_Solutions_P1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
