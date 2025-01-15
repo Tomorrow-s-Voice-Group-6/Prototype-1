31a519a4915d6987b3c8aa7721d35c8cfffe9b0b
CarterWilliams7 <cwilliams54@ncstudents.niagaracollege.ca>
2025-01-14 20:32:57 -0500

    Added first model, created initial database and migration.

--- TVAttendance/TVAttendance/Program.cs
+++ TVAttendance/TVAttendance/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
 
 builder.Services.AddDbContext<TomorrowsVoiceContext>(options =>
     options.UseSqlite(connectionString));
