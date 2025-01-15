728293385f7e7a4e9906ae87fdd1adf82b55ec8b
CarterWilliams7 <cwilliams54@ncstudents.niagaracollege.ca>
2025-01-14 20:55:36 -0500

    Added data annotations to Singer model

--- TVAttendance/TVAttendance/Models/Singer.cs
+++ TVAttendance/TVAttendance/Models/Singer.cs
@@ -1,17 +1,44 @@
-﻿namespace TVAttendance.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TVAttendance.Models
 {
     public class Singer
     {
+        [Key]
         public int ID { get; set; }
+        [Display(Name = "First Name")]
+        [MaxLength(55)]
+        [Required]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(100)]
+        [Required]
         public string LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Required]
         public DateTime DOB { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        [MaxLength(255)]
         public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Required]
         public string Phone { get; set; }
+
+        [Display(Name = "Status")]
         public bool Status { get; set; }
+
+        [Display(Name = "Register Date")]
         public DateOnly RegisterDate { get; set; }
+        [Display(Name = "Emergency Contact First Name")]
         public string EmergencyContactFirstName { get; set; }
+        [Display(Name = "Emergency Contact Last Name")]
         public string EmergencyContactLastName { get; set; }
+        [Display(Name = "Emergency Contact Phone")]
         public string EmergencyContactPhone { get; set; }
 
         /*public Chapter ChapterID { get; set; } *///Foreign key
