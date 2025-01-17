9531f51606a007a7b3d463359994c031701f061c
feddymichel <feddymichel@gmail.com>
2025-01-16 20:57:45 -0500

    Resolved merge conflicts and staged changes

--- TVAttendance/TVAttendance/obj/Debug/net8.0/TVAttendance.AssemblyInfo.cs
+++ TVAttendance/TVAttendance/obj/Debug/net8.0/TVAttendance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TVAttendance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224777634f1ac0c78275be30c0ac12a1268658af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228533bafeeeede83ad8153904d4f244d659e8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TVAttendance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TVAttendance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
