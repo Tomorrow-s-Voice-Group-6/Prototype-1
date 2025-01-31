df79f9d14beb6c0df314db11957f2f2f8603a6ec
CarterWilliams7 <cwilliams54@ncstudents.niagaracollege.ca>
2025-01-31 14:15:25 -0500

    Refreshed .gitignore by removing cached files

---
+++ TVAttendance/TVAttendance/Data/Migrations/20250129204057_attendance.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TVAttendance.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class attendance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
