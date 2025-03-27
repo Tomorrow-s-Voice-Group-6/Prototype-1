20cd6cee9968d516057218d56e536a2bfb6a453f
CarterWilliams7 <cwilliams54@ncstudents.niagaracollege.ca>
2025-03-27 14:29:13 -0400

    Director crud fixes
    - Fixed create modal
    - Added chapters to director create
    - Added chapters to director edit
    - Fixed pagination for director
    - Removed useless details.css page
    - Fixed fill director script

--- TVAttendance/TVAttendance/wwwroot/js/FillDirectorFields.js
+++ TVAttendance/TVAttendance/wwwroot/js/FillDirectorFields.js
@@ -1,6 +1,7 @@
-﻿document.getElementById("fill").addEventListener("click", function () {
-    document.getElementById("dir-first").value = "Janet"
-    document.getElementById("dir-last").value = "Stone"
-    document.getElementById("dir-email").value = "JStone@hotmail.com"
-    document.getElementById("phoneNumber").value = "2890391873"
-}) 
+﻿document.getElementById("fill").addEventListener("click", function (e) {
+    e.preventDefault(); // Prevents the form from being submitted
+    document.getElementById("dir-first").value = "Janet";
+    document.getElementById("dir-last").value = "Stone";
+    document.getElementById("dir-email").value = "JStone@hotmail.com";
+    document.getElementById("phoneNumber").value = "2890391873";
+});
